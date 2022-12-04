f7d66b4618c834bf3c3e2a44548024744925d180
GRRTBOW <grrtbaw@gmail.com>
2022-12-04 20:17:31 +1100

    redeisigning page and adding click to player buttons

---
+++ script.js
@@ -0,0 +1,8 @@
+let paperBtn = document.querySelector('.paper-btn');
+let playerPick = document.querySelector('.player-pick')
+
+
+paperBtn.addEventListener('click', () => {
+    playerPick.textContent = "paper"; 
+});
+
