278a80842be2afc556660e02945b41db8435539c
GRRTBOW <grrtbaw@gmail.com>
2022-12-09 18:07:36 +1100

    adding interactive buttons

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
-let paperBtn = document.querySelector('.paper-btn');
-let playerPick = document.querySelector('.player-pick')
+let startBtn = document.querySelector('.start-btn');
+let gameScreen = document.querySelector('.game-screen');
+let menu = document.querySelector('.choices')
+let choices = Array.from(document.querySelectorAll('.choices'));
+let playerChoice = document.querySelector('.player-pick');
+let playerCount = 0;
+let computerCount = 0;
 
 
-paperBtn.addEventListener('click', () => {
-    playerPick.textContent = "paper"; 
-});
+function startGame(){
+    console.log(startBtn.innerText);
+    if (startBtn.innerText == "RESET"){
+        startBtn.innerText = "START";
+        gameScreen.style.display = 'none';
+        menu.style.display = 'none';
+    } else if (startBtn.innerText == "START"){
+        gameScreen.style.display = 'flex';
+        menu.style.display = 'flex';
+        startBtn.innerText = "RESET";
+    }
+}
+
+function showPlayerChoice(e){
+    playerChoice.innerText = `${e.target.className}`;
+    showComputerChoice();
+    updateCount()
+}
+
+function showComputerChoice(){
+    let computerDisplay = document.querySelector('.computer-pick');
+    let computerPick = getRandomInt(3);
+    console.log(computerPick);
+
+    if (computerPick == 0){
+        computerDisplay.innerText = "paper";
+    } else if (computerPick == 1){
+        computerDisplay.innerText = "scissors";
+    } else {
+        computerDisplay.innerText = "rock";
+    }
+} 
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+
+function updateCount(){
+    
+}
+startBtn.addEventListener('click', startGame);
+choices.forEach(choice => choice.addEventListener('click', showPlayerChoice));
 
